5e96804ef9d7e95ef942cc5540792184922dae6e
EkaterinaBurshtyn <57365129+EkaterinaBurshtyn@users.noreply.github.com>
2019-11-22 18:14:50 +0300

    Add files via upload

---
+++ Pr_1_4/src/MaxMin.java
@@ -0,0 +1,8 @@
+public class MaxMin {
+    static void max_min() {
+        float max = Float.MAX_VALUE;
+        float min = Float.MIN_VALUE;
+        System.out.println("max="+ max);
+        System.out.print("min="+ min);
+    }
+}
