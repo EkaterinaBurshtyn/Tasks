ef2c046750802c5c2fcd233dacd21df26bc6b5f7
EkaterinaBurshtyn <57365129+EkaterinaBurshtyn@users.noreply.github.com>
2019-11-26 14:06:06 +0300

    Delete MaxMin.java

--- Pr_1_4/src/MaxMin.java
+++
@@ -1,8 +0,0 @@
-public class MaxMin {
-    static void max_min() {
-        float max = Float.MAX_VALUE;
-        float min = Float.MIN_VALUE;
-        System.out.println("max="+ max);
-        System.out.print("min="+ min);
-    }
-}
