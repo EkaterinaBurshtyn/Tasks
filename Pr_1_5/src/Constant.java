15cd8a5f104c2a96442fa756552f5336cdfa04a2
EkaterinaBurshtyn <57365129+EkaterinaBurshtyn@users.noreply.github.com>
2019-11-26 14:08:42 +0300

    Add files via upload

---
+++ Pr_1_5/src/Constant.java
@@ -0,0 +1,4 @@
+public class Constant {
+    public static int const1=0XF0;
+    public static int const2=0XA1;
+}
---
+++ Pr_1_5/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String [] args){
+        System.out.println("const1="+Constant.const1+ ", const2="+Constant.const2);
+        System.out.println("const1&const2="+Integer.toBinaryString(Constant.const1&Constant.const2));
+
+    }
+}
