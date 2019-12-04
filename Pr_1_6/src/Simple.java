e2d9bb2a0966b4337295c7ab8f70e390c430bbdd
EkaterinaBurshtyn <57365129+EkaterinaBurshtyn@users.noreply.github.com>
2019-12-04 15:34:47 +0300

    Update Simple.java

--- Pr_1_6/src/Simple.java
+++ Pr_1_6/src/Simple.java
@@ -1,9 +1,4 @@
 public class Simple {
-     int n;
-
-    Simple(int n){
-        this.n=n;
-    }
     public static boolean simple_number(int n){
         for (int i=2;i<n;i++){
             if (n%i==0)
