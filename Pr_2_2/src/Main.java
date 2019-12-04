6fa13702848daf64368ac96c0dc4f04070b47b62
EkaterinaBurshtyn <57365129+EkaterinaBurshtyn@users.noreply.github.com>
2019-12-04 15:32:27 +0300

    Add files via upload

---
+++ Pr_2_2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String [] args){
+        Integer a = 7;
+        Double b= 5.25;
+        Integer s=(int)(a+b);
+        Double ss= a+b;
+        System.out.println(s+"//"+ss);
+
+    }
+}
