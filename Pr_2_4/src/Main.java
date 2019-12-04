318819de3774f1daecfa9aa972086545cbf842c9
EkaterinaBurshtyn <57365129+EkaterinaBurshtyn@users.noreply.github.com>
2019-12-04 15:33:23 +0300

    Add files via upload

---
+++ Pr_2_4/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String [] args){
+        int b;
+        int a =1409;
+        while (a!=0){
+            b=a%2;
+            System.out.print(b );
+            a=a/2;
+        }
+    }
+}
